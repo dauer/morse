9fe2a78c1588a37d52752af46a647043cd63b47f
Thomas Rasmussen <thomas.bregentved@gmail.com>
2011-08-22 15:16:16 +0200

    Added readme file

---
+++ README.mk
@@ -0,0 +1,13 @@
+Morse
+==========
+
+Converts plain text to norsecode
+
+Usage
+-----
+    $echo Hello world | ./morse.py
+    ...././.-../.-../—-//.—/—-/.-./.-../-..
+
+Letter spacing = `/´
+
+Word spacing = `//´
